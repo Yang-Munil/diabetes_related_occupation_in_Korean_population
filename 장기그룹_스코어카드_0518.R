4f6a0e2d1b45afe631dab2a405ee7b2fd84bbf34
Yang-Munil <66587987+Yang-Munil@users.noreply.github.com>
2021-06-22 20:55:37 +0900

    Add files via upload

--- 장기그룹_스코어카드_0518.R
+++ 장기그룹_스코어카드_0518.R
@@ -1,18 +1,17 @@
-
 library(ggplot2)
 library(Rprofet)
 
 
-df2 <- read.csv("C:/rproject/df2_columns_select_final.csv")
+df2 <- read.csv("C:/rproject/df2_columns_select_49.csv")
 
 
 # 장기
 
 
-binned <- BinProfet(df2, id= "TPR_NO_ENC", target= "TARGET", num.bins = 5) ## Binning variables
+binned <- BinProfet(df2, id= "TPR_NO_ENC", target= "TARGET", num.bins = 3) ## Binning variables
 
 
-WOE_dat <- WOEProfet(binned, "TPR_NO_ENC", "TARGET", 3:24)
+WOE_dat <- WOEProfet(binned, "TPR_NO_ENC", "TARGET", 3:48)
 
 
 
@@ -29,9 +28,12 @@ write.csv(df2_scorecard, "df2_scorecard_final.csv")
 
 # 장기그룹 파일럿 변수 시각화
 
-names(binned)
+a <- names(binned)
+a
 
-WOEplotter(binned, target= "TARGET", var= "A07_SNP_AMT_DUE_3Y_Bins")
+WOEplotter(binned, target= "TARGET", var= c("A07_SNP_AMT_DUE_1Y_Bins",
+                                            "A07_SNP_AMT_DUE_FIX_1Y_Bins",
+                                            "A07_SNP_IN_ACCT_VR_1Y_Bins"))
 
 WOEplotter(binned, target= "TARGET", var= "A07_SNP_CNT_DUE_FIX_3Y_Bins")
 
@@ -80,7 +82,7 @@ WOEplotter(binned, target= "TARGET", var= "A02_CNP_CNT_3YB_Bins")
 df2_score$TARGET <- as.factor(df2_score$TARGET)
 
 ggplot(df2_score, aes(x=Score, fill = TARGET, color = TARGET)) +
-  geom_histogram(aes(y=..density..), alpha=0.5, position = "dodge", binwidth = 15) +
+  geom_histogram(aes(y=..density..), alpha=0.5, position = "dodge", binwidth = 10) +
   theme(legend.position = "top") +
   theme_minimal() +
   theme(legend.position = "top") +
@@ -94,52 +96,17 @@ ggplot(df2_score, aes(x=Score, fill = TARGET, color = TARGET)) +
 
 
 
+library(InformationValue)
+
+# obser
+ks_plot(actuals=df2_score$TARGET, predictedScores=df2_score$Score)
+
+ks_stat(actuals=df2_score$TARGET, predictedScores=df2_score$Score)
 
 
 
 
-# 단기
 
-
-binned <- BinProfet(df1, id= "TPR_NO_ENC", target= "TARGET", num.bins = 5) ## Binning variables
-
-
-WOE_dat <- WOEProfet(binned, "TPR_NO_ENC", "TARGET", 3:40)
-
-
-head(WOE_dat$BinWOE)
-
-head(WOE_dat$WOE)
-
-WOE_dat$IV
-
-Score_dat <- ScorecardProfet(WOE_dat, target="TARGET",
-                             id= "TPR_NO_ENC", PDO = 50, BaseOdds = 10, BasePts = 1000, reverse = TRUE)
-
-df1_scorecard <- Score_dat$Scorecard
-
-df1_score <- Score_dat$Results
-
-# 단기그룹 체납자, 수납자
-
-df1_score$TARGET <- as.factor(df1_score$TARGET)
-
-ggplot(df1_score, aes(x=Score, fill = TARGET, color = TARGET)) +
-  geom_histogram(aes(y=..density..), alpha=0.5, position = "dodge", binwidth = 15) +
-  theme(legend.position = "top") +
-  theme_minimal() +
-  theme(legend.position = "top") +
-  ggtitle("단기그룹 체납자, 수납자 스코어 분포") +
-  theme(plot.title = element_text(family = "serif", face = "bold", hjust = 0.5, size = 20, color = "black")) +
-  labs(x="스코어", y="인원수") +
-  theme(axis.title = element_text(family = "serif", face = "bold", hjust = 0.5, size = 20, color = "black"))
-
-
-
-
-# 스코어기준점 추출
-
-write.csv(df1_scorecard, "df1_scorecard.csv")
 
 
 
